0be453d10e5c7c720028f1f1c12d593ada327941
mozibrishu <mozib.risu@gmail.com>
2023-04-13 11:48:49 +0600

    Done designing

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 document.querySelector('.convertBtn').addEventListener('click', () => {
-  inputText = document.querySelector('.textInput').value;
+  let inputText = document.querySelector('.textInput').value;
   console.log(inputText)
-  dataCh = inputText.split(/\n/);
+  let dataCh = inputText.split(/\n/);
   document.querySelector('.textResult').value = '';
   for (i = 0; dataCh[i]; i++) {
-    channel2 = dataCh[i];
+    let channel2 = dataCh[i];
     fetching(channel2);
   }
 
@@ -12,18 +12,18 @@ document.querySelector('.convertBtn').addEventListener('click', () => {
     fetch(`https://yt.jaybee.digital/api/channels?part=channels&handle=${channel2}`)
       .then(response => response.json())
       .then(data => {
-        if(!(data.items[0].id)){
-          newUrl = `https://www.youtube.com/channel/${data.items[0].id}\n`;
+        let newUrl = `https://www.youtube.com/channel/${data.items[0].id}\n`;
+        if ((data.items[0].id)) {
           document.querySelector('.textResult').value += newUrl;
         }
-
         document.querySelector('.linkTableBody').innerHTML += `<tr>
     <td><a href="${newUrl}" target='_blank'>${channel2}</a></td>
     <td>${newUrl}</td>
   </tr>`
       })
       .catch(err => {
-        newUrl = `Not Found`;
+        console.log(err);
+        let newUrl = `Not Found`;
         document.querySelector('.textResult').value += newUrl;
 
         document.querySelector('.linkTableBody').innerHTML += `<tr>
